9b71800876b2fd326b2316ac808b0741c5e7fe99
Itamar Junior <itamazor@gmail.com>
2019-12-06 22:24:14 -0300

    correct controller

--- src/controller/home.js
+++ src/controller/home.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var router = express.Router()
 
-var UrlVideo = require('../Models/UrlVideo')
+var UrlVideo = require('../models/UrlVideo')
 
 router.get('/', async (req, res) => {
   try {
